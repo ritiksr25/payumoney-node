f8e660a6e21a0953d4142459a81c160a95d49875
ritik srivastava <ritiksrivastava025@gmail.com>
2019-10-28 22:23:52 +0530

    correction

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require("./config/dbconnection");
-const Payment = require("./models/Payment");
+require('./config/dbconnection');
 
-app.use("/", require("./routes/index"));
+const Payment = require('./models/Payment');
+
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT;
 app.listen(PORT, err => {
-	if (err) console.log("Error in running Server.");
+	if (err) console.log('Error in running Server.');
 	else console.log(`Server is up and running on Port ${PORT}`);
 });
